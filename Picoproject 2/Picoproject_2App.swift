aba0ce7c7cb0c9d435a69a81dc1c8f8844277448
Marcus-Gallardo <mgallardo7007@gmail.com>
2023-02-12 11:19:53 -0800

    Initial Commit

---
+++ Picoproject 2/Picoproject_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Picoproject_2App.swift
+//  Picoproject 2
+//
+//  Created by Marcus Gallardo on 2/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Picoproject_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
